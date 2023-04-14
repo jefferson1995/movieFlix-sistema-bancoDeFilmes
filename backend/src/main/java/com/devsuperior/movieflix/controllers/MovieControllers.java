3dc780a8efc93ceceb9cf6bf98365b4bb2965827
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-04-14 20:58:11 -0300

    Endpoint: movies paged

---
+++ backend/src/main/java/com/devsuperior/movieflix/controllers/MovieControllers.java
@@ -0,0 +1,32 @@
+package com.devsuperior.movieflix.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.services.MovieService;
+
+@RestController
+@RequestMapping(value = "/movies")
+public class MovieControllers {
+
+	@Autowired
+	private MovieService service;
+
+	@GetMapping
+	public ResponseEntity<Page<MovieDTO>> findAllPaged(Pageable pageable) {
+
+		//Retorna por ordem de nome
+		PageRequest pageRequest = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Sort.by("title"));
+		Page<MovieDTO> page = service.findAllPaged(pageRequest);
+
+		return ResponseEntity.ok().body(page);
+	}
+}
---
+++ backend/src/main/java/com/devsuperior/movieflix/services/MovieService.java
@@ -0,0 +1,26 @@
+package com.devsuperior.movieflix.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.MovieDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+
+@Service
+public class MovieService {
+
+	@Autowired
+	private MovieRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<MovieDTO> findAllPaged(Pageable pageable) {
+
+		Page<Movie> list = repository.findAll(pageable);
+
+		return list.map(x -> new MovieDTO(x));
+	}
+}
